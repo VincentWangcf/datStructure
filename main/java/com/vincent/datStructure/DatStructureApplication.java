0823ce4953a7640f2ff167e2f49c6d81c6c5beef
445047477 <445047477@qq.com>
2022-05-29 12:20:36 +0800

    demo

---
+++ main/java/com/vincent/datStructure/DatStructureApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.datStructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatStructureApplication.class, args);
+	}
+
+}
