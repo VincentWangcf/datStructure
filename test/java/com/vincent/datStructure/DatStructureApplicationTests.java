eb023d32ce1878ddcb2f3ccc2b8921f4acd13a11
445047477 <445047477@qq.com>
2022-07-29 22:52:07 +0800

    添加代码

---
+++ test/java/com/vincent/datStructure/DatStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.datStructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
